b981e9dac27c47ec5c8c16e481a583922c2447da
niranjangirhe <niranjangirheindia@gmail.com>
2021-06-25 22:35:42 +0530

    added MainActitviy and ModeSelectAcitvity

---
+++ app/src/main/java/com/ngsolutions/champhulya/ModeSelectAcitvity.java
@@ -0,0 +1,14 @@
+package com.ngsolutions.champhulya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModeSelectAcitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_select_acitvity);
+    }
+}
